8994bf18ba5f542804f85193ecceed5ef8b11049
Augustine Okeke <hursteen@gmail.com>
2019-10-14 13:38:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from './CardList'
+import {robots} from './robots'
+import SearchBox from './SearchBox'
+const App = () => {
+    return (
+        <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox/>
+            <CardList robots={robots} />
+        </div>
+    );
 }
 
 export default App;
+
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz',
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Anthonette',
+        email: 'shanna@melissa.tv',
+    },
+    {
+        id: 3,
+        name: 'Clementine Clementine',
+        username: 'samantha',
+        email: 'Clementine.Clementine@yesenia.net',
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Patricia.Lebsack@april.biz',
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Bret',
+        email: 'Chelsey.Dietrich@april.biz',
+    },
+    {
+        id: 6,
+        name: 'Paul Eistein',
+        username: 'Eistein',
+        email: 'Paul.Eistein@april.biz',
+    },
+    {
+        id: 7,
+        name: 'Steve peter',
+        username: 'peter',
+        email: 'Steve.peter@april.biz',
+    },
+    {
+        id: 8,
+        name: 'Smith John',
+        username: 'Smith',
+        email: 'Smith.John@april.biz',
+    },
+    {
+        id: 9,
+        name: 'Abraham Isaac',
+        username: 'Isaac',
+        email: 'Abraham.Isaac@april.biz',
+    },
+    {
+        id: 10,
+        name: 'Yetunde Adebola',
+        username: 'Yetunde',
+        email: 'Yetunde Adebola@april.biz',
+    },
+]
