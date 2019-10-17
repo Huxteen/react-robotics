becab09714186d371ea34612e8cecff60222038f
Augustine Okeke <hursteen@gmail.com>
2019-10-17 18:45:18 +0100

    search function

--- src/CardList.js
+++ src/CardList.js
@@ -6,7 +6,7 @@ const CardList = ({robots}) => {
         <div>
             {
                 robots.map((user, i) => {
-                    return <Card 
+                    return <Card
                                 key={i} 
                                 id={robots[i].id} 
                                 name={robots[i].name} 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const SearchBox = () => {
+const SearchBox = ({searchChange}) => {
     return (
         <div className="pa2">
             <input 
                 className="pa3 ba b--green bg-lightest-blue tc"
                 type="search"
                 placeholder="search robots"
+                onChange={searchChange}
             />
         </div>
     );
