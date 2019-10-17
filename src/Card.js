16deec3a8af9499dda4ba8b4f9f03e4413fdd482
Augustine Okeke <hursteen@gmail.com>
2019-10-17 19:16:23 +0100

    custom design

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Card.css';
 
 
 const Card = ({name, email, id}) => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+        <div className="card-bg dib br3 pa3 ma2 grow bw2 shadow-5 tc">
             <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
             <div>
                 <h2>{name}</h2>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SearchBox.css';
 
 
 const SearchBox = ({searchChange}) => {
     return (
-        <div className="pa2">
+        <div className="pa2 search-container">
             <input 
-                className="pa3 ba b--green bg-lightest-blue tc"
+                className="pa3 search-border ba bg-lightest-blue tc"
                 type="search"
                 placeholder="search robots"
                 onChange={searchChange}
