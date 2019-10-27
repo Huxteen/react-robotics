dd808cc5ff71cbed1fd1cd8f058b745918ac7346
Augustine Okeke <hursteen@gmail.com>
2019-10-27 22:37:30 +0100

    update

--- src/action.js
+++ src/action.js
@@ -1,4 +1,4 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
